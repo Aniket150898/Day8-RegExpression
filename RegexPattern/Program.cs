de094333b50b621c9f2d1bedd9ef73829a0ac7ec
Aniket150898 <103835629+Aniket150898@users.noreply.github.com>
2023-03-29 16:18:47 +0530

    [Aniket]- print welcome message

---
+++ RegexPattern/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome!!,Performing Regual Expression in C#");
+            Console.ReadLine();
+        }
+    }
+}
